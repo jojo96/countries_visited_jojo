9b94697eff971154f9d785dd67f779a63e9fed4f
Ujjayanta <aprajit09041996@gmail.com>
2023-07-06 23:47:41 +0200

    Add files via upload

---
+++ cities.js
@@ -0,0 +1,19 @@
+CITIES =
+[
+   // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
+   {name: 'London', latitude: 51.507351, longitude: -0.127758, radius: 3, fillKey: 'city'},
+   {name: 'Cardiff', latitude: 51.481583, longitude: -3.179090, radius: 3, fillKey: 'city', date: '2019-9'},
+   {name: 'Gent', latitude: 51.054340, longitude: 3.717424, radius: 3, fillKey: 'city'},
+   {name: 'Amsterdam', latitude: 52.370216, longitude: 4.895168, radius: 3, fillKey: 'city'},
+   {name: 'Brussels', latitude: 50.850346, longitude: 4.351721, radius: 3, fillKey: 'city'},
+   {name: 'Paris', latitude: 48.856614, longitude: 2.352222, radius: 3, fillKey: 'city'},
+   {name: 'Cologne', latitude: 50.937531, longitude: 6.960279, radius: 3, fillKey: 'city'},
+   {name: 'Oslo', latitude: 59.913869, longitude: 10.752245, radius: 3, fillKey: 'city'},
+   {name: 'Sarajevo', latitude: 43.856259, longitude: 18.413076, radius: 3, fillKey: 'city'},
+   {name: 'Rhodes', latitude: 36.434053, longitude: 28.217638, radius: 3, fillKey: 'city'},
+   {name: 'Budapest', latitude: 47.497912, longitude: 47.497912, radius: 3, fillKey: 'city'},
+   {name: 'Bucharest', latitude: 44.426767, longitude: 26.102538, radius: 3, fillKey: 'city'},
+   {name: 'Prague', latitude: 50.075538, longitude: 14.4378, radius: 3, fillKey: 'city'},
+   {name: 'Krakow', latitude: 50.06465, longitude: 19.94498, radius: 3, fillKey: 'city'},
+   {name: 'Istanbul', latitude: 41.008238, longitude: 28.978359, radius: 3, fillKey: 'city'}
+]
---
+++ countries.js
@@ -0,0 +1,20 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+COUNTRIES =
+{
+    IND: {fillKey: 'visited'},
+    SGP: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+	BEL: {fillKey: 'visited'},
+    LUX: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+	FRA: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    NOR: {fillKey: 'visited'},
+	BIH: {fillKey: 'visited'},
+    GRC: {fillKey: 'visited'},
+    HUN: {fillKey: 'visited'},
+	ROU: {fillKey: 'visited'},
+    CZE: {fillKey: 'visited'},
+    POL: {fillKey: 'visited'},
+	TUR: {fillKey: 'visited'}
+}
